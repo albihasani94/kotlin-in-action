dee96de585644deaa0dc13e83f215097ebd5df1c
albihasani94 <albihasani94@gmail.com>
2020-08-13 00:56:11 +0200

    initial commit

---
+++ src/App.kt
@@ -0,0 +1,9 @@
+
+data class Person (val name: String, val age: Int? = null)
+
+fun main() {
+    val persons = listOf(Person("Alice"), Person("BoB", 29))
+
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("The oldest person is $oldest")
+}
