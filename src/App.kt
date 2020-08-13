3d534d75a8b2f935db6afa2f1f242ea7a2ef56f5
albihasani94 <albihasani94@gmail.com>
2020-08-13 02:03:34 +0200

    Add functions

--- src/App.kt
+++ src/App.kt
@@ -1,8 +1,24 @@
 data class Person(val name: String, val age: Int? = null)
 
 fun main() {
+    // Java 9 for ya
     val persons = listOf(Person("Alice"), Person("BoB", 29))
 
-    val oldest = persons.maxBy { it.age ?: 0 }
+    val oldest = findOldest(persons)
     println("The oldest person is $oldest") // The oldest person is Person(name=BoB, age=29)
+
+    // Look at this nice string templating
+    val a = 5
+    val b = 6
+    println("Max of $a and $b is ${max(a, b)}")
+}
+
+// What a taste of lambda
+fun findOldest(persons: List<Person>): Person? {
+    return persons.maxBy { it.age ?: 0 }
+}
+
+// Functions are fun
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
 }
